f1ea9deb38cd345f6f2a10df65329f3c31488d09
Jeff Everhart <jeffeverhart383@gmail.com>
2024-02-05 10:39:29 -0500

    update homepage

---
+++ app/block-kit/page.tsx
@@ -0,0 +1,34 @@
+import getChannels from "../lib/slack/getChannels";
+import SlackIntegration from "./components/SlackIntegration";
+import { sendVideoMessage } from "../actions/sendVideoMessage";
+import { sendPlainTextMessage } from "../actions/sendPlainTextMessage";
+import { sendAlertMessage } from "../actions/sendAlertMessage";
+
+export default async function Home() {
+  const isConfigured = process.env.SLACK_TOKEN ? true : false;
+  const channels = await getChannels();
+  const actions = {
+    sendVideoMessage,
+    sendPlainTextMessage,
+    sendAlertMessage,
+  };
+  console.log(channels);
+  return (
+    <main className="flex flex-col content-center m-6">
+      <h1 className="text-2xl font-bold">Slack App Playground</h1>
+      <section>
+        {isConfigured ? (
+          <>
+            <span>✅ Slack Token Found</span>
+            <SlackIntegration
+              channels={channels}
+              actions={actions}
+            ></SlackIntegration>
+          </>
+        ) : (
+          <span>❌ Missing Slack Token</span>
+        )}
+      </section>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,17 @@
-import Image from "next/image";
-import getChannels from "./lib/slack/getChannels";
-import SlackIntegration from "./components/SlackIntegration";
-import { sendVideoMessage } from "./actions/sendVideoMessage";
-import { sendPlainTextMessage } from "./actions/sendPlainTextMessage";
-import { sendAlertMessage } from "./actions/sendAlertMessage";
+import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
-  const isConfigured = process.env.SLACK_TOKEN ? true : false;
-  const channels = await getChannels();
-  const actions = {
-    sendVideoMessage,
-    sendPlainTextMessage,
-    sendAlertMessage,
-  };
-  console.log(channels);
   return (
     <main className="flex flex-col content-center m-6">
       <h1 className="text-2xl font-bold">Slack App Playground</h1>
-      <section>
-        {isConfigured ? (
-          <>
-            <span>✅ Slack Token Found</span>
-            <SlackIntegration
-              channels={channels}
-              actions={actions}
-            ></SlackIntegration>
-          </>
-        ) : (
-          <span>❌ Missing Slack Token</span>
-        )}
-      </section>
+      <Link
+        href="/block-kit"
+        className={`${buttonVariants({ variant: "outline" })} mt-6`}
+      >
+        Block Kit Examples
+      </Link>
     </main>
   );
 }
